bf6feb0020986f0cd0679e5a97d1b9a5ed542e55
cintiatrindadecoelho <ctc3@aluno.ifnmg.edu.br>
2026-04-28 19:26:34 +0000

    feat: terminei o exercicio 8

--- exercicios/aula08/utils/texto.js
+++ exercicios/aula08/utils/texto.js
@@ -3,5 +3,7 @@
 // ========================================
 // Implemente a função e exporte com "export default".
 export default function formatarNome(nome) {
-  // escreva seu código aqui
+  const primeiraLetra = nome.charAt(0).toUpperCase();
+  const resto = nome.slice(1).toLowerCase();
+  return primeiraLetra + resto;
 }
